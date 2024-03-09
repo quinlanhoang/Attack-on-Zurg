bf87167f702d553d862ba6fec6f1cc5964d3bce5
amhsiung <amhsiung@ucsc.edu>
2024-03-09 00:42:48 -0800

    added loading bar

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -4,6 +4,20 @@ class Load extends Phaser.Scene {
     }
 
     preload() {
+
+        // loading bar
+        // see: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/loader/
+        let loadingBar = this.add.graphics();
+        this.load.on('progress', (value) => {
+            loadingBar.clear();                                 // reset fill/line style
+            loadingBar.fillStyle(0xFFFFFF, 1);                  // (color, alpha)
+            loadingBar.fillRect(0, centerY, w * value, 5);  // (x, y, w, h)
+        });
+        this.load.on('complete', () => {
+            loadingBar.destroy();
+        });
+
+        
         //implement credits later
         
         this.load.path = './assets/';
